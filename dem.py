2b977f0de8db76dc8786fa8d12e3116c092c8186
{shivansh} <{goelshivansh8343@gmail.com}>
2025-06-29 16:04:29 +0530

    Shanu

--- dem.py
+++ dem.py
@@ -1,1 +1,10 @@
-print("hello world")
+"""This module prints a greeting message."""
+
+
+def main():
+    """Prints hello world"""
+    print("hello world")
+
+
+if __name__ == "__main__":
+    main()
